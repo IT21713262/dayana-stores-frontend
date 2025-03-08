59890e4eb247cd52fbe5932c69924266aa6cbb03
thilinisewwandi416 <thilini.sewwandi416@gmail.com>
2025-03-08 22:56:22 +0530

    updating ignore and optimizing routes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ import AddEmployee from "./Components/Employee/AddEmployee";
 import ViewEmployee from "./Components/Employee/ViewEmployee";
 import EditEmployee from "./Components/Employee/EditEmployee";
 import OrderManagementDashboard from "./Components/OrderManagement/OrderManagementDashboard";
-
-/*import GenerateReport from "./Components/Suppliers/GenerateReport";
-import * as Inventory from "./Components/Inventory/InventoryRoutes"; */
+import * as Inventory from "./Components/Inventory/InventoryRoutes";
 import LoginPage from '../src/Components/Users/auth/LoginPage'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './Components/Users/common/Navbar';
@@ -31,56 +29,50 @@ function App() {
      <AuthProvider>
         <div className="App">
           <Navbar />
-          <div className="content">
-      <Routes>
-        {/*user routes */}
-        <Route exact path="/" element={<LoginPage />} />
-              <Route exact path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegistrationPage />} /> {/* Now accessible to all users */}
-              <Route path="/profile" element={<ProfilePage />} />
+        <div className="content">
+        <Routes>
+          {/*user routes */}
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} /> {/* Now accessible to all users */}
+          <Route path="/profile" element={<ProfilePage />} />
 
-              {/*Check if user is authenticated and admin, before rendering admin-only routes*/}
-              {UserService.adminOnly() && (
-                <>
-                  
-                  <Route path="/admin/user-management" element={<UserManagementPage />} />
-                  <Route path="update-user/:userId" element={<UpdateUser />} />
-                </>
-              )}
-              <Route path='*' element={<Navigate to="/login" />} />%
-        {/*supplier routes*/}
-        <Route path="/supplierDashboard" element={<SupplierDashboard />} />
-        <Route path="/addSuppliers" element={<AddSupplier />} />
-        <Route path="/updateSuppliers/:id" element={<UpdateSupplier />} />
-        <Route path="/transactions" element={<SupplierTransactions />} />
-        <Route path="/addTransaction" element={<AddTransaction />} />
+          {/*Check if user is authenticated and admin, before rendering admin-only routes*/}
+            {UserService.adminOnly() && (
+            <>
+              <Route path="/admin/user-management" element={<UserManagementPage />} />
+              <Route path="update-user/:userId" element={<UpdateUser />} />
+            </>
+          )}
+          <Route path='*' element={<Navigate to="/login" />} />%
+          
+          {/*supplier routes*/}
+          <Route path="/supplierDashboard" element={<SupplierDashboard />} />
+          <Route path="/addSuppliers" element={<AddSupplier />} />
+          <Route path="/updateSuppliers/:id" element={<UpdateSupplier />} />
+          <Route path="/transactions" element={<SupplierTransactions />} />
+          <Route path="/addTransaction" element={<AddTransaction />} />
+          <Route path="/supplierReport" element={<SupplierReport />} />
+          <Route path="/viewSupplier/:id" element={<ViewSupplier />} />
 
-        <Route path="/supplierReport" element={<SupplierReport />} />
-        <Route path="/viewSupplier/:id" element={<ViewSupplier />} />
+          {/*Employee routes */}
+          <Route path="/EmployeeDashboard" element={< EmployeeDashboard />} />
+          <Route path="/AddEmployee" element={<AddEmployee />} />
+          <Route path="/ViewEmployee/:id" element={<ViewEmployee />} />
+          <Route path="/EditEmployee/:id" element={<EditEmployee />} />
 
-         {/*Employee routes */}
-        <Route path="/EmployeeDashboard" element={< EmployeeDashboard />} />
-        <Route path="/AddEmployee" element={<AddEmployee />} />
-        <Route path="/ViewEmployee/:id" element={<ViewEmployee />} />
-        <Route path="/EditEmployee/:id" element={<EditEmployee />} />
+          {/*Orders*/}
+          <Route path="/OrderManagementDashboard" element={<OrderManagementDashboard />} />
 
-        {/*Orders*/}
-        <Route path="/OrderManagementDashboard" element={<OrderManagementDashboard />} />
+          {/*inventory routes */}
+          <Route path="/InventoryDashboard" element={<Inventory.InventoryDashboard />} />
+          <Route path="/add-item" element={<Inventory.AddNewItem/>} />
 
-        
-        <Route path="/generateReport" element={<GenerateReport />} />
-        <Route path="/" element={<SupplierDashboard />} />
-
-        {/*inventory routes */}
-        <Route path="/InventoryDashboard" element={<Inventory.InventoryDashboard />} />
-        <Route path="/add-item" element={<Inventory.AddNewItem/>} />
-
-
-      </Routes>
-      </div>
-          <Footer />
+        </Routes>
         </div>
-        </AuthProvider>
+            <Footer />
+        </div>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
